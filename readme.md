2b5c80a4e7af05e32adce3a18c928c3936339a29
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-04 15:20:47 -0800

    fix

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Julius Alvarado
+## www.julius3d.com 
+
+* Just building a blog using PHP ^_^
+
+## License
+
+open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ resources/views/about.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>www.julius3d.com</title>
+
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+
+<body>
+
+<div class="content">
+    <h1>Julius Alvarado</h1>
+    <h2>About my PHP Development services:</h2>
+    <p class="links">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci animi consectetur deleniti dolorem facere ipsa
+        laboriosam quas. Accusantium aperiam cumque debitis earum expedita non odit qui, quo reprehenderit. Saepe.
+    </p>
+</div>
+
+
+</body>
+</html>
